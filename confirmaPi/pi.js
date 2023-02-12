bac0657b6daefcf2dda2e117d572ad5ea1c900c6
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-02-12 17:09:30 -0300

    create pi.js

---
+++ confirmaPi/pi.js
@@ -0,0 +1,9 @@
+//3.14159
+let minhaResposta = prompt("Qual é o valor de PI com 5 casas decimais?");
+let respostaConvertida = Number(minhaResposta);
+
+if (respostaConvertida == 3.14159){
+  alert("Acertou!");
+} else {
+  alert("Errou!");
+}
