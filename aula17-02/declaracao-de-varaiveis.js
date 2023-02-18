01327c6415c13d19909512b07916f1a8c7508e5b
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-02-17 21:57:32 -0300

    Aprendendo sobre declarações de variáveis e escopos

---
+++ aula17-02/declaracao-de-varaiveis.js
@@ -0,0 +1,12 @@
+const gostaDeFrio = true;
+let nome = "Ranieri"
+let sobrenome;
+
+
+if(gostaDeFrio) {
+  nome = "Ranieri Oliveira";
+  alert("Gosto de frio");
+}
+
+alert(nome);
+alert(sobrenome);
