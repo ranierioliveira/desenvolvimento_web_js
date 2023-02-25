80f7444ce3fd99829949e93dc252023cd8c3fa14
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-02-24 22:00:16 -0300

    Anotação de alguns conceitos da aula de js

---
+++ aula24-02/declarandoVariaveis.js
@@ -0,0 +1,6 @@
+const nome = "Ranieri";
+const sobrenome = "Oliveira";
+//const nomeCompleto = `${nome} ${sobrenome}`;
+//const nomeCompleto = nome.concat(' ', sobrenome);
+const nomeCompleto = (nome + " " + sobrenome);
+alert(nomeCompleto);
---
+++ aula24-02/trabalhandoComNumeros.js
@@ -0,0 +1,16 @@
+const idade = 60_500;
+const pi = 3.14;
+const temperaturaDaPatagonia = -15.10;
+
+let a = 5;
+a++
+//alert(a);
+a--
+//alert(a);
+
+let d = 5;
+let b = 2;
+
+d *=b; 
+
+alert(d);
